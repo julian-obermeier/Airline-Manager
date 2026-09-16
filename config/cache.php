6ea2b39776408323912706be8ffe1c1d5799df9d
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 21:42:15 +0200

    Use file cache by default for shared hosting

--- config/cache.php
+++ config/cache.php
@@ -3,12 +3,20 @@
 use Illuminate\Support\Str;
 
 return [
-    'default' => env('CACHE_STORE', 'redis'),
+    'default' => env('CACHE_STORE', 'file'),
+
     'stores' => [
         'array' => [
             'driver' => 'array',
             'serialize' => false,
         ],
+
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
+        ],
+
         'database' => [
             'driver' => 'database',
             'connection' => env('DB_CACHE_CONNECTION'),
@@ -16,11 +24,13 @@ return [
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
             'lock_table' => env('DB_CACHE_LOCK_TABLE'),
         ],
+
         'redis' => [
             'driver' => 'redis',
             'connection' => env('REDIS_CACHE_CONNECTION', 'cache'),
             'lock_connection' => env('REDIS_CACHE_LOCK_CONNECTION', 'default'),
         ],
     ],
+
     'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'airline-empire')).'-cache-'),
 ];
