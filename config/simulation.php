bd0498164aedfea9b8c58ac92723fabaf7abe28b
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 22:37:44 +0200

    Configure booking demand window

--- config/simulation.php
+++ config/simulation.php
@@ -3,5 +3,6 @@
 return [
     'cron_token' => env('SIMULATION_CRON_TOKEN'),
     'boarding_minutes' => (int) env('SIMULATION_BOARDING_MINUTES', 30),
+    'booking_window_days' => (int) env('SIMULATION_BOOKING_WINDOW_DAYS', 14),
     'fuel_price_minor_per_liter' => (int) env('SIMULATION_FUEL_PRICE_MINOR_PER_LITER', 88),
 ];
