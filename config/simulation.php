3408857c60afc6a359f0b1b68191bd8abe3def82
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 22:47:55 +0200

    Configure operational delay evaluation

--- config/simulation.php
+++ config/simulation.php
@@ -4,5 +4,6 @@ return [
     'cron_token' => env('SIMULATION_CRON_TOKEN'),
     'boarding_minutes' => (int) env('SIMULATION_BOARDING_MINUTES', 30),
     'booking_window_days' => (int) env('SIMULATION_BOOKING_WINDOW_DAYS', 14),
+    'delay_evaluation_minutes' => (int) env('SIMULATION_DELAY_EVALUATION_MINUTES', 180),
     'fuel_price_minor_per_liter' => (int) env('SIMULATION_FUEL_PRICE_MINOR_PER_LITER', 88),
 ];
