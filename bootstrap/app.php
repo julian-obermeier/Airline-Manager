75e7356e0a7e006380f8a003272845c3a68d16b0
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 23:49:38 +0200

    Register aircraft position middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureAircraftAtRouteOrigin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->alias([
+            'aircraft.position' => EnsureAircraftAtRouteOrigin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Domain-specific exception rendering will be added per module.
