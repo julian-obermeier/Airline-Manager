cf7058ea4e1bef58f817c1ca4925468aa5d79eb1
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 09:37:33 +0200

    Update tests for centralized fleet and pricing routes

--- tests/Feature/FlightSimulationTest.php
+++ tests/Feature/FlightSimulationTest.php
@@ -55,7 +55,7 @@ class FlightSimulationTest extends TestCase
 
         $this->createQualifiedCrew($airline, $type, $frankfurt);
 
-        $this->post(route('operations.fleet.purchase'), [
+        $this->post(route('fleet-market.new'), [
             'aircraft_type_id' => $type->id,
             'registration' => 'D-ASIM',
         ]);
