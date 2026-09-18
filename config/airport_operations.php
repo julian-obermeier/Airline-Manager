048eabd4e9a19f8187376b804140baf8f7f42ef2
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 08:57:12 +0200

    Add airport stations and slot foundation

---
+++ config/airport_operations.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'slot_bucket_minutes' => 15,
+    'default_slot_capacity_per_bucket' => 12,
+
+    'station_monthly_minor' => [
+        'base' => 75000000,
+        'outstation' => 12000000,
+    ],
+
+    'slot_fee_minor' => 20000,
+    'base_movement_fee_minor' => 50000,
+    'per_seat_movement_fee_minor' => 250,
+    'per_passenger_service_fee_minor' => 450,
+];
